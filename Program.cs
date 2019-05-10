7474d57ec906cedf038fa45b3fd7a65bcd152ae6
Phillip Thain <pthain@purdue.edu>
2019-05-10 15:54:46 -0400

    Initialize C# Hello World project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FocusManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
