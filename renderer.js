32b3fba995dcd5e283573326ae09487f6e84e0d5
Phillip Thain <pthain@purdue.edu>
2019-06-08 09:22:54 -0400

    Add textarea and echo input as a dialog

--- renderer.js
+++ renderer.js
@@ -1,3 +1,18 @@
-const {BrowserWindow} = require('electron').remote
+const {BrowserWindow, dialog} = require('electron').remote
 
 console.log("This is the renderer process.")
+
+const echoTextarea = document.getElementById('input-textarea')
+const echoBtn = document.getElementById('echo-btn')
+
+echoBtn.addEventListener('click', (event) => {
+  console.log('Echoing text ...')
+  const options = {
+    type: 'info',
+    title: 'Echo',
+    message: echoTextarea.value,
+    buttons: ['Acknowledge']
+  }
+  console.log(echoTextarea.value)
+  dialog.showMessageBox(options)
+})
