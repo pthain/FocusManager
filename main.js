ce0258f688102a36c065ee3268aad4cbc10da2bb
Phillip Thain <pthain@purdue.edu>
2019-06-12 17:38:15 -0400

    Add read/write ability to top menu

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
   Credits to the electron-quick-start project.
 ***/
 
-const {app, BrowserWindow, Menu, shell} = require('electron')
+const {app, BrowserWindow, dialog, Menu, shell} = require('electron')
 
 //Global reference to the window object
 let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow ({
-    width:  600,
-    height: 400,
+    width:  1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
@@ -19,32 +19,38 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   //Open dev tools on startup
-  //mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   mainWindow.on('close', () => {
     console.log("Closing all windows...")
     mainWindow = null
   })
 }
-
+/*
 let template = [{
   role: 'fileMenu',
   submenu: [{
     label: 'Save',
-    accelerator: 'CmdOrCtrl+Z',
+    accelerator: 'CmdOrCtrl+s',
     click: () => {
       console.log("Time to save a file")
+      saveTextToFile()
     }
   }]
 }, {
   role: 'viewMenu'}]
 
+function saveTextToFile() {
+  dialog.showSaveDialog(null, (path) => {
+    console.log(path)
+//    fs.writeFile(path, )
+  })
+
+
+}*/
 //app.on('ready', createWindow)
 app.on('ready', () => {
   createWindow()
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
-
 })
 
 app.on('window-all-closed', () => {
--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
-const {BrowserWindow, dialog} = require('electron').remote
+const {app, BrowserWindow, dialog, Menu, shell} = require('electron').remote
+const fs = require('fs')
 
 console.log("This is the renderer process.")
 
@@ -16,3 +17,50 @@ echoBtn.addEventListener('click', (event) => {
   console.log(echoTextarea.value)
   dialog.showMessageBox(options)
 })
+
+let template = [{
+  role: 'fileMenu',
+  submenu: [{
+    label: 'Save',
+    accelerator: 'CmdOrCtrl+s',
+    click: () => {
+      console.log("Time to save a file")
+      saveTextToFile()
+    }
+  }, {
+    label: 'Open',
+    accelerator: 'CmdOrCtrl+o',
+    click: () => {
+      console.log("Time to open a file")
+      openTextFile()
+    }
+  }]
+}, {
+  role: 'viewMenu'
+}]
+
+function openTextFile() {
+  var paths = dialog.showOpenDialog((paths) => {
+    if (paths === undefined) console.log("No file selected")
+    console.log(paths[0])
+    fs.readFile(paths[0], "utf-8", (err, data) => {
+      if (err) throw err;
+      echoTextarea.value = data
+      console.log('Successful read.')
+    })
+  })
+}
+
+function saveTextToFile() {
+  dialog.showSaveDialog(null, (path) => {
+    console.log(path)
+    fs.writeFile(path, echoTextarea.value, (err) => {
+      if (err) throw err;
+      console.log('Successful save.')
+    })
+  })
+
+}
+//app.on('ready', createWindow)
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
